96de05c3dd35d9ce7a03affe5873b294f540f49c
Mostafafathyeid <66316400+Mostafafathyeid@users.noreply.github.com>
2022-01-06 18:34:52 +0200

    Add files via upload

---
+++ Project 1/wgrep.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char **arg)
+{
+    if (argc == 1)
+    {
+        cout << "wgrep: searchterm [file ...]" << endl;
+        return 1;
+    }
+    FILE *file = freopen(arg[2], "r", stdin);
+    if (argc == 3)
+    {
+        if (file == NULL)
+        {
+            cout << "wgrep: cannot open file" << endl;
+            return 1;
+        }
+    }
+    string cmp = arg[1];
+    string line;
+    char c;
+    bool check=0;
+    while (c != EOF)
+    {
+        if (c == '\n'){
+            c = getchar();
+            continue;
+        }
+        getline(cin, line);
+        line = c + line;
+        if (line.find(cmp) != string::npos)
+        {
+            line.erase(line.begin());
+            if (check)
+                cout << c;
+            cout << line << endl;
+        }
+        c = getchar();
+        check=1;
+    }
+    return 0;
+}

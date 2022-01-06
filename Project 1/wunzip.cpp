351eb09d91a5c08f5129b38fa8f71f9cd6bde5be
yossef-ahmed-231 <66273666+yossef-ahmed-231@users.noreply.github.com>
2022-01-06 22:59:02 +0200

    Add files via upload

--- Project 1/wunzip.cpp
+++ Project 1/wunzip.cpp
@@ -14,18 +14,18 @@ int main(int argc, char** argv)
     {
         FILE *file ;
         file = freopen(argv[i],"r",stdin);
-        vector<pair<char,int>> res;
-        char c;
-        int rep = 0;
-        while(fread(&rep,1,4,file)){
+        vector<pair<char,int>> res;              /// save answer in res
+        char c;                                  /// character to read each character from file
+        int rep = 0;                             /// variable to count the no of repeation of each character 
+        while(fread(&rep,4,1,file)){             /// reads the block of data (4byte) from the stream
             c = getchar();
-            res.push_back({c,rep});
+            res.push_back({c,rep});              /// store the character and its repeation in res
         }
-        for(auto it:res){
+        for(auto it:res){                        /// loop to output the result
             for(int j = 0; j < it.second ;++j)
                 cout << it.first;
         }
-        fclose(file);
+        fclose(file);                            /// close the opened file
     }
     return 0;
 }

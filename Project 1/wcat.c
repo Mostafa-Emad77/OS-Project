89ff45807435abf2f5dcb72b76983b02eabb817a
yossef-ahmed-231 <66273666+yossef-ahmed-231@users.noreply.github.com>
2022-01-06 23:24:29 +0200

    Update wcat.c

--- Project 1/wcat.c
+++ Project 1/wcat.c
@@ -10,16 +10,14 @@ int main(int argc, char* argv[]){
     FILE* fp;
     char ch;
     for(i = 1; i < argc; i++){
-      fp = fopen(argv[i], "r");
-      // case of issue opening file
+      fp = fopen(argv[i], "r");        // case of issue opening file
       if(fp == NULL){
         printf("wcat: cannot open file\n");
         return 1;
       }
-      // case that file has been opened, iterate
-      // through file and output contents
-      while((ch = fgetc(fp)) != EOF){
-        printf("%c", ch);
+     
+      while((ch = fgetc(fp)) != EOF){ // case that file has been opened, iterate through file and output contents
+        printf("%c", ch);             // print each char in the file
       }
 
       fclose(fp);

c0850b2efe35fd36e4f19072747532d253189e9a
yossef-ahmed-231 <66273666+yossef-ahmed-231@users.noreply.github.com>
2022-01-07 13:35:31 +0200

    Update wzip.c

--- Project 1/wzip.c
+++ Project 1/wzip.c
@@ -10,51 +10,61 @@
 
 int main(int argc,char** argv)
 {
-int x=0;
-char* str =(char*)malloc(1);
-if (argc ==1) {
-printf("wzip: file1 [file2 ...]\n");
-return 1;
-}
-int k;
-for(k=1;k<argc;k++){
-FILE *fileread=fopen(argv[k],"r");
-if(fileread==NULL){
-    printf("ERROR FILE OPENING\n");
-    exit(1);
-}
-else{
+	int x=0;
+	
+	char* str =(char*)malloc(1);
+	
+	if (argc ==1) {
+		
+	printf("wzip: file1 [file2 ...]\n");
+	return 1;
+		
+	}
+	
+	int k;
+	
+	for(k=1;k<argc;k++){
+		
+		FILE *fileread=fopen(argv[k],"r");
 
-      // reading line by line, max 256 bytes
+		if(fileread==NULL){
 
-	char c;
-    while ((c = fgetc(fileread)) != EOF){
-    	x++;
-    	str=realloc(str,x+1);
-    	str[x-1]=c;
-    	str[x]='\0';
-    }
-    
-    fclose(fileread);
+		    printf("ERROR FILE OPENING\n");
+		    exit(1);
 
-    }
+		}else{
 
-}
+		char c;
+
+		while ((c = fgetc(fileread)) != EOF){              // reading char by char.
+
+			x++;
+			str=realloc(str,x+1);
+			str[x-1]=c;                                 //append new character in the array
+			str[x]='\0';                                //add '\0' at the end 
+		}
+
+		fclose(fileread);
+
+		}
+
+	}
        	
-     int i;
-    int j=1;
-    for(i=0;((str[i]) != '\0'); i++){
+        int i;
+        int j=1;
+	
+  	for(i=0;((str[i]) != '\0'); i++){
     	
-        if((str[i])!=(str[i+1])){
-            times=j;
-            ch=str[i];
-            fwrite(&times,sizeof(int),1,stdout);
-            fwrite(&ch,sizeof(char),1,stdout);
-            j=1;
-            }
+		if((str[i])!=(str[i+1])){
+		    times=j;
+		    ch=str[i];
+		    fwrite(&times,sizeof(int),1,stdout);    //print times of character appearence.
+		    fwrite(&ch,sizeof(char),1,stdout);      //print the character in the file.
+		    j=1;
+		}
 
-    else j++;
-    }
+		 else j++;
+    	}
 	
 
 }
